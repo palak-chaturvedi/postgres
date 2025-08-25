2b67f25aaa6a80664acc1fc3626e919e3a591139
Ashutosh Bapat <ashutosh.bapat.oss@gmail.com>
2025-08-25 19:23:50 +0530

    Add a view to read contents of shared buffer lookup table
    The view exposes the contents of the shared buffer lookup table for
    debugging, testing and investigation.
    
    This helped me in debugging issues where the buffer descriptor array and
    buffer lookup table were out of sync; either the buffer lookup table had
    a mapping page->buffer which wasn't present in the buffer descriptor
    array or a page in the buffer descriptor array didn't have corresponding
    entry in the buffer lookup table. pg_buffercache doesn't help with those
    kind of issues. Also doing that under the debugger in very painful.
    
    I intend to keep this patch while the rest of the code matures. If it is
    found useful as a debugging tool, we may consider make it committable
    and commit it.
    
    Author: Ashutosh Bapat <ashutosh.bapat.oss@gmail.com>

--- contrib/pg_buffercache/pg_buffercache_pages.c
+++ contrib/pg_buffercache/pg_buffercache_pages.c
@@ -77,6 +77,7 @@ PG_FUNCTION_INFO_V1(pg_buffercache_evict_all);
 PG_FUNCTION_INFO_V1(pg_buffercache_mark_dirty);
 PG_FUNCTION_INFO_V1(pg_buffercache_mark_dirty_relation);
 PG_FUNCTION_INFO_V1(pg_buffercache_mark_dirty_all);
+PG_FUNCTION_INFO_V1(pg_buffercache_lookup_table_entries);
 
 
 /* Only need to touch memory once per backend process lifetime */
@@ -921,4 +922,20 @@ pg_buffercache_mark_dirty_all(PG_FUNCTION_ARGS)
 	result = HeapTupleGetDatum(tuple);
 
 	PG_RETURN_DATUM(result);
+}
+
+/*
+ * Return lookup table content as a set of records.
+ */
+Datum
+pg_buffercache_lookup_table_entries(PG_FUNCTION_ARGS)
+{
+	ReturnSetInfo *rsinfo = (ReturnSetInfo *) fcinfo->resultinfo;
+
+	InitMaterializedSRF(fcinfo, 0);
+
+	/* Fill the tuplestore */
+	BufTableGetContents(rsinfo->setResult, rsinfo->setDesc);
+
+	return (Datum) 0;
 }
